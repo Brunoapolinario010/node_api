f5dd0bba05474fb1a0f3758cc226e62652a52e65
Bruno Apolinário <bruno.apolinario010@gmail.com>
2023-11-23 14:16:53 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const PORT = parseInt(process.env.PORT) || 3000;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/api', require('./router/userRoutes'));
+
+app.listen(PORT, () => {
+	console.log(`Server is listening on port ${PORT}`);
+});
